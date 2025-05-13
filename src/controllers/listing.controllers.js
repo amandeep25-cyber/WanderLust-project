fd29cbc9743ffb6d9bcdf2c8672151b32b0bedbe
amandeep25 <st.amandeep25@gmail.com>
2025-05-13 23:15:23 +0530

    Creating a new listing route

--- src/controllers/listing.controllers.js
+++ src/controllers/listing.controllers.js
@@ -1,11 +1,39 @@
 const Listing = require("../models/listing.models");
 
-const getAllListing = async(req,res)=>{
-    const allData = await Listing.find({});
-    
-    res.render("listing/index.ejs",{allData});
-}
+const getAllListing = async (req, res) => {
+  const allData = await Listing.find({});
 
-module.exports= {
-    getAllListing,
-}
+  res.render("listing/index.ejs", { allData });
+};
+
+const getListing = async (req, res) => {
+  const { id } = req.params;
+  const list = await Listing.findById(id);
+  res.render("listing/show.ejs", { list });
+};
+
+const createForm = (req, res) => {
+  res.render("listing/new.ejs");
+};
+
+const creatingListing = async (req, res) => {
+  const { title, location, country, description, price } = req.body;
+  const newListing = new Listing({
+    title,
+    description,
+    price,
+    country,
+    location,
+  });
+
+  const createdListing = await newListing.save();
+
+  res.redirect("/listing");
+};
+
+module.exports = {
+  getAllListing,
+  getListing,
+  createForm,
+  creatingListing,
+};
--- src/routes/listing.routes.js
+++ src/routes/listing.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllListing } = require("../controllers/listing.controllers")
+const {
+  getAllListing,
+  getListing,
+  createForm,
+  creatingListing,
+} = require("../controllers/listing.controllers");
 
-router.route('/').get(getAllListing);
-
+router.route("/").get(getAllListing);
+router.route("/new").get(createForm);
+router.route("/:id").get(getListing);
+router.route("/").post(creatingListing);
 
 module.exports = router;
